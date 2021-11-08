ace91aef60b549b47ee3d854ed10566f15068b7d
Shubham Bandagale <sbandagale18@gmail.com>
2021-11-08 21:36:35 +0530

    ADDED:GET Request method and pass name as query parameter

--- src/main/java/com/example/restapi_hello/controller/HelloRestController.java
+++ src/main/java/com/example/restapi_hello/controller/HelloRestController.java
@@ -1,13 +1,29 @@
 package com.example.restapi_hello.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
-@RequestMapping(value = {"","/","hello"})
-    public String sayHello(){
+    @RequestMapping(value = {"", "/", "hello"})
+    /**
+     *localhost:8080/hello
+     */
+    public String sayHello() {
         return "Hello from Bridgelabz !!";
     }
+
+    /**
+     *localhost:8080/hello/query?name=Shubham
+     */
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String name){
+        return "Hello" + name + "!!!" ;
+    }
+
+    
+
 }
